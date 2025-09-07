6d423d299fccf7b75e9ab4d7e2c2d96b34e6f5c8
xadicha <mahkamovahadija1810@gmail.com>
2025-09-07 14:04:10 +0500

    pharmacist service's query fixed

--- pharmacist-service/query.sql
+++ pharmacist-service/query.sql
@@ -1,3 +1,5 @@
+---------------- Medicine CRUD ----------------
+
 -- name: CreateMedicine :one
 INSERT INTO medicines
     (
@@ -18,12 +20,22 @@ RETURNING
     company,
     status,
     created_at,
-    updated_at,
-    deleted_at;
+    updated_at;
 
 -- name: GetMedicineById :one
-SELECT * FROM medicines
-WHERE id=$1;
+SELECT 
+    id,
+    name,
+    category,
+    company,
+    status,
+    price,
+    created_at,
+    updated_at, 
+FROM 
+    medicines
+WHERE id=$1
+    AND deleted_at IS NULL;
 
 -- name: ListMedicines :many
 SELECT
@@ -35,10 +47,11 @@ SELECT
     price,
     created_at,
     updated_at,
-    COUNT(*) OVER() AS total_count  -- jami yozuvlar soni filter bo'yicha
+    COUNT(*) OVER() AS total_count  -- total count filtering boyicha
 FROM
     medicines
 WHERE
+    deleted_at IS NULL
     (:search IS NULL OR -- :search - frontenddan kelgan parametr. NULL bolsa, barcha datalarni qaytaradi. Agar bosh bolmasa name, category, company ustunlarida qidirishni boshlaydi.
         LOWER(name) LIKE LOWER(CONCAT('%', :search, '%')) OR --LOWER - barcha harflarni kichkina qilib oladi. 
         LOWER(category) LIKE LOWER(CONCAT('%', :search, '%')) OR -- CONCAT('%', :search, '%') - example: %aspirin% qilib qidiradi, yani matn ichida qayerdan bolmasin topadi.
@@ -60,6 +73,7 @@ SET
     company = $6,
     status =$7,
 WHERE id = $1
+    AND deleted_at IS NULL
 RETURNING
     id, 
     name,
@@ -69,11 +83,75 @@ RETURNING
     company,
     status,
     created_at,
-    updated_at,
-    deleted_at;
+    updated_at;
 
--- name: DeleteMedicine :one
+-- name: DeleteMedicine :exec
 UPDATE medicines
 SET deleted_at = $2
 WHERE id = $1;
 
+---------------- Medicine Category CRUD ----------------
+
+-- name: CreateMedicineCategory :one
+INSERT INTO medicine_categories
+    (
+        name,
+        description
+    )
+VALUES ($1, $2)
+RETURNING
+    id,
+    name,
+    description,
+    created_at,
+    updated_at;
+
+-- name: GetMedicineCategoryById :one
+SELECT 
+    name,
+    description,
+    created_at,
+    updated_at,
+FROM 
+    medicine_categories
+WHERE id=$1;
+
+-- name: ListMedicineCategories :many
+SELECT
+    name,
+    description,
+    created_at,
+    updated_at,
+    COUNT(*) OVER() AS total_count
+FROM 
+    medicine_categories
+WHERE 
+    deleted_at IS NULL
+    (
+        :search IS NULL OR
+        LOWER(name) LIKE LOWER(CONCAT('%', :search, '%'))
+    )
+ORDER BY
+    created_at DESC
+LIMIT :limit 
+OFFSET :offset; 
+
+-- name: UpdateMedicineCategory :one 
+UPDATE medicine_categories
+SET 
+    name = $2,
+    category = $3,
+    description = $4,
+WHERE id=$1
+    AND deleted_at IS NULL
+RETURNING
+    id,
+    name,
+    description,
+    created_at,
+    updated_at;
+
+-- name DeleteMedicineCategory :exec
+UPDATE medicine_categories
+SET deleted_at = $2
+WHERE id = $1;
