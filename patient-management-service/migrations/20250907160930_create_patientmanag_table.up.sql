5c8dcfcdfcbf8d36c4bd6d28b788348189561670
xadicha <mahkamovahadija1810@gmail.com>
2025-09-08 15:16:33 +0500

    nurse and patient management services' migration have been written

--- patient-management-service/migrations/20250907160930_create_patientmanag_table.up.sql
+++ patient-management-service/migrations/20250907160930_create_patientmanag_table.up.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS patients(
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) UNIQUE NOT NULL,
     address TEXT NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
     gender gender_type NOT NULL,
     birth_date DATE NOT NULL,
     blood_group blood_type NOT NULL,
